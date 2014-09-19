af52b25cfb6db399ccdaffadf777cd69499b9bfe
Sumner McCarty <sumner@rightscale.com>
2014-09-19 17:05:32 -0400

    CA-4108 - Gem.available? lies so wrapped the require differently

--- lib/sql_partitioner.rb
+++ lib/sql_partitioner.rb
@@ -1,11 +1,3 @@
-# method for testing gem availability across Ruby 1.87 and 2+
-def gem_available?(name)
-   Gem::Specification.find_by_name(name)
-rescue Gem::LoadError
-   false
-rescue
-   Gem.available?(name)
-end
 
 # standard requires
 require 'sql_partitioner/sql_helper'
@@ -13,7 +5,10 @@ require 'sql_partitioner/partitions_manager'
 require 'sql_partitioner/adv_partitions_manager'
 
 # only require AR adapter if AR available
-require 'sql_partitioner/adapters/ar_adapter' if gem_available?("activerecord")
-
+begin
+  require 'sql_partitioner/adapters/ar_adapter'
+rescue
+  # had to wrap it this way as Gem.available? lies
+end
 
 
