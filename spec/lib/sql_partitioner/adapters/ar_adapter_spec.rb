3eafc60cedc28d74d3cf201ba2f2a8309a6fadd5
Dominic Metzger <dominic@rightscale.com>
2014-09-25 15:52:35 -0700

    added spec coverage for ar_adapter

---
+++ spec/lib/sql_partitioner/adapters/ar_adapter_spec.rb
@@ -0,0 +1,59 @@
+require File.expand_path("../../spec_helper", File.dirname(__FILE__))
+
+shared_examples_for "an adapter" do
+  before(:each) do
+  end
+  describe ".select" do
+    context "with multiple columns selected" do
+      context "and no data returned" do
+        it "should return an empty hash" do
+          adapter.select("SELECT * FROM EVENTS").should == []
+        end
+      end
+      context "and data returned" do
+        before(:each) do
+          adapter.execute("INSERT INTO events(timestamp) VALUES(1)")
+          adapter.execute("INSERT INTO events(timestamp) VALUES(2)")
+        end
+        it "should return a Struct with accessors for each column" do
+          result = adapter.select("SELECT * FROM events")
+
+          result.should be_a_kind_of(Array)
+
+          result.first.should be_a_kind_of(Struct)
+          result.first.timestamp.should == "1"
+
+          result.last.should be_a_kind_of(Struct)
+          result.last.timestamp.should  == "2"
+        end
+      end
+    end
+    context "with a single column selected" do
+      before(:each) do
+        adapter.execute("INSERT INTO events(timestamp) VALUES(1)")
+        adapter.execute("INSERT INTO events(timestamp) VALUES(2)")
+      end
+      it "should return the values without Struct" do
+        result = adapter.select("SELECT timestamp FROM events")
+
+        result.should be_a_kind_of(Array)
+
+        result.first.should == "1"
+        result.last.should  == "2"
+      end
+      it "should return an array even when only one row is selected" do
+        result = adapter.select("SELECT timestamp FROM events LIMIT 1")
+
+        result.should be_a_kind_of(Array)
+      end
+    end
+  end
+end
+
+describe SqlPartitioner::ARAdapter do
+  it_should_behave_like "an adapter" do
+    let(:adapter) do 
+      SqlPartitioner::ARAdapter.new(ActiveRecord::Base.connection)
+    end
+  end
+end
