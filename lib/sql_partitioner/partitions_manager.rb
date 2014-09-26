b267f1c6b12f82336008a6ad9a4fb041a72a3b48
Sumner McCarty <sumner@rightscale.com>
2014-09-26 11:33:27 -0400

    CA-4108 - removed days_in_past from init, and made a few other minor changes/comments

--- lib/sql_partitioner/partitions_manager.rb
+++ lib/sql_partitioner/partitions_manager.rb
@@ -2,9 +2,9 @@ module SqlPartitioner
   class PartitionsManager < BasePartitionsManager
 
     # Initialize the partitions based on intervals relative to current timestamp.
-    # Number of partition will be equal to the number of (:months or :days) provided.
+    # Number of partition will be equal to the number of 
+    # (intervals_per_partition.partition_interval, i.e. 1.month) partitions needed to cover days_into_future.
     #
-    # @param [Fixnum] Number of days into the past from current_timestamp
     # @param [Fixnum] Number of days into the future from current_timestamp
     # @param [Symbol] partition interval type [:months, :days]
     # @param [Fixnum] number of intervals per partition, i.e. 3 month partitions
@@ -14,22 +14,16 @@ module SqlPartitioner
     # @return [String] sql to initialize partitions if dry run is true
     # @raise [ArgumentError] if days is not array or if one of the
     #                    days is not integer
-    def initialize_partitioning_in_intervals(days_in_past, days_into_future = 27, partition_interval = :months, partition_size = 1, dry_run = false)
-      _validate_initialize_partitioning_in_intervals_params(days_in_past, days_into_future)
+    def initialize_partitioning_in_intervals(days_into_future, partition_interval = :months, intervals_per_partition = 1, dry_run = false)
+      _validate_initialize_partitioning_in_intervals_params(days_into_future)
 
-      current_date_time = @tum.from_time_unit_to_date_time(current_timestamp)
-      starting_date_time = TimeUnitManager.advance_date_time(current_date_time, :days, -days_in_past)
-      @current_timestamp = @tum.to_time_unit(starting_date_time.to_time.to_i)
-
-      partition_data = partitions_to_append(@current_timestamp, partition_interval, partition_size, days_in_past + days_into_future)
+      partition_data = partitions_to_append(@current_timestamp, partition_interval, intervals_per_partition, days_into_future)
       initialize_partitioning(partition_data, dry_run)
     end
 
-    def _validate_initialize_partitioning_in_intervals_params(parts_before, parts_from_now)
-      [parts_before, parts_from_now].each do |parts|
-        msg = "parts should be Fixnum but #{parts.class} found"
-        _raise_arg_err(msg) unless parts.kind_of?(Fixnum)
-      end
+    def _validate_initialize_partitioning_in_intervals_params(days_into_future)
+      msg = "days_into_future should be Fixnum but #{days_into_future.class} found"
+      _raise_arg_err(msg) unless days_into_future.kind_of?(Fixnum)
       true
     end
     private :_validate_initialize_partitioning_in_intervals_params
@@ -38,17 +32,17 @@ module SqlPartitioner
     # Get partition to add a partition to end with the given window size
     #
     # @param [Symbol] partition_interval: [:days, :months]
-    # @param [Fixnum] partition_size, intervals covered by the new partition
+    # @param [Fixnum] intervals_per_partition, intervals covered by the new partition
     # @param [Fixnum] partitions_into_future, how many partitions into the future should be covered
     #
     # @return [Hash] partition_data hash
     VALID_PARTITION_INTERVALS = [:months, :days]
-    def partitions_to_append(partition_start_timestamp, partition_interval, partition_size, days_into_future)
+    def partitions_to_append(partition_start_timestamp, partition_interval, intervals_per_partition, days_into_future)
       if partition_interval.nil? || !VALID_PARTITION_INTERVALS.include?(partition_interval)
         _raise_arg_err "partition_interval must be one of: #{VALID_PARTITION_INTERVALS.inspect}"
       end
-      if partition_size.nil? || partition_size <= 0
-        _raise_arg_err "partition_size should be > 0"
+      if intervals_per_partition.nil? || intervals_per_partition <= 0
+        _raise_arg_err "intervals_per_partition should be > 0"
       end
       if days_into_future.nil? || days_into_future <= 0
         _raise_arg_err "partitions_into_future should be > 0"
@@ -62,7 +56,7 @@ module SqlPartitioner
       new_partition_data    = {}
 
       while latest_part_date_time < date_time_to_be_covered
-        latest_part_date_time = TimeUnitManager.advance_date_time(latest_part_date_time, partition_interval, partition_size * 1)
+        latest_part_date_time = TimeUnitManager.advance_date_time(latest_part_date_time, partition_interval, intervals_per_partition)
 
         new_partition_ts   = @tum.to_time_unit(latest_part_date_time.strftime('%s').to_i)
         new_partition_name = name_from_timestamp(new_partition_ts)
@@ -76,21 +70,21 @@ module SqlPartitioner
     # given window size
     #
     # @param [Symbol] partition_interval: [:days, :months]
-    # @param [Fixnum] partition_size, intervals covered by the new partition
+    # @param [Fixnum] intervals_per_partition, intervals covered by the new partition
     # @param [Fixnum] partitions_into_future, how many partitions into the future should be covered
     # @param [Boolean] dry run, default value is false. Query wont be executed
     #                  if dry_run is set to true
     # @return [Boolean] true if dry_run is false
     # @return [String] reorg sql if dry run is true
     # @raise [ArgumentError] if  window size is nil or not greater than 0
-    def append_partition_intervals(partition_interval, partition_size, days_into_future = 27, dry_run = false)
+    def append_partition_intervals(partition_interval, intervals_per_partition, days_into_future = 30, dry_run = false)
       partitions = Partition.all(adapter, table_name)
       if partitions.blank?
         raise "partitions must be properly initialized before appending"
       end
       latest_partition = partitions.latest_partition
 
-      new_partition_data = partitions_to_append(latest_partition.timestamp, partition_interval, partition_size, days_into_future)
+      new_partition_data = partitions_to_append(latest_partition.timestamp, partition_interval, intervals_per_partition, days_into_future)
 
       if new_partition_data.empty?
         msg = <<-MSG
